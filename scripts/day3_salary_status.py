d228e552885e80090d92d6ee5afc971c2be417bd
Andleeb Khan <andleebk857@gmail.com>
2025-12-27 08:07:52 +0500

    day3_salary_status.py

---
+++ scripts/day3_salary_status.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("employees.csv")
+
+# employees have salaries above department avg.
+df["salary"] = df["salary"].round(2)
+df["dept_avg_salary"] = df.groupby("department")["salary"].transform("mean").round(2)
+employees = df.loc[df["salary"]>df["dept_avg_salary"], "name"].unique()
+employees_above_avg = ", ".join(employees)
+print("These employees have salaries above average")
+print(employees_above_avg)
+
+# salary status
+df["salary_status"] = np.where(df["salary"]>df["dept_avg_salary"], "High", "Low")
+
+# save ouput
+df.to_csv("employees_salary_analysis.csv", index=False)
+print("Department analysis completed successfully.")
