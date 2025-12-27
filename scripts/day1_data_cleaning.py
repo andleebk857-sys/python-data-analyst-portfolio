378655ecdcf0e6d6faa0df62ab20bfca70817b8d
Andleeb Khan <andleebk857@gmail.com>
2025-12-27 08:30:55 +0500

    day1_data_cleaning.py

---
+++ scripts/day1_data_cleaning.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from rapidfuzz import process, fuzz
+df = pd.read_csv("employees.csv")
+print("first 5 rows")
+print(df.head())
+print("info")
+print(df.info())
+print("null")
+print(df.isnull().sum())
+# fill missing values 
+df["department"] = df["department"].fillna("Unknown")
+df["salary"] = df["salary"].fillna(df.groupby("department")["salary"].transform("mean")).round(2)
+# standardize the department names
+department_names = ["IT", "HR", "Finance", "Human_Resource", "Sales"]
+def correct(names):
+    if pd.isna(names):
+        return None
+    names = names.strip().title()
+    match, score, _ = process.extractOne(names, department_names, scorer=fuzz.ratio)
+    if score>=40:
+        return match
+    else:
+        return None
+df["department"] = df["department"].apply(correct)
+df.to_csv("cleaned_employee.csv", index=False)
+print(df.isnull().sum())
+print(df["department"].value_counts())
